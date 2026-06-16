690de41076b5035180933dea345d24ca60b8963f
Madhavi30-create <madhavimangal3009@gmail.com>
2026-06-16 20:19:38 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello!") we are using node.js not browser
+
+let name = "madhavi"
+let age = 21
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned yet
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // output undefined
+console.log(typeof null); // output object
+
+
+
+
