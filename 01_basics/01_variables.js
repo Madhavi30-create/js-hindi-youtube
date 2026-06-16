21cc337f3d1b6246b104fbf77578cf6e374a95cc
Madhavi30-create <madhavimangal3009@gmail.com>
2026-06-16 20:19:21 +0000

    const var let

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 30905
+let accountEmail = "madhavimangal3009@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 270304 this assignment is not allowed
+/*
+prefer not to use var because of issue in block scope and functional scope
+*/
+
+accountEmail = "rakhibansal1983@gmail.com"
+accountPassword = "21112"
+accountCity = "Delhi"
+
+console.log([accountEmail, accountPassword, accountId, accountCity, accountState]);
+
+
+
